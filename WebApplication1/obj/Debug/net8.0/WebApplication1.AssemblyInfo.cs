0ac74c5dd62ff4b58b88a552a96b44d4f5df5044
Walid <54804554+WalidTa@users.noreply.github.com>
2026-01-09 17:36:04 +0100

    laatste styling. app is nu klaar.

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ccee821983667fa831ab976b4cba0e6a073648c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c8653044936970217d88b36d4838682030876a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
